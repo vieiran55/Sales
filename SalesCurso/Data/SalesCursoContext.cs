21ebfb8db40e943426798eb43da39bea4d6ee98f
Antonio Vieira <vieira.n55@gmail.com>
2023-12-05 17:23:02 -0300

    CRUD Scaffolding

---
+++ SalesCurso/Data/SalesCursoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesCurso.Models;
+
+namespace SalesCurso.Data
+{
+    public class SalesCursoContext : DbContext
+    {
+        public SalesCursoContext (DbContextOptions<SalesCursoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesCurso.Models.Department> Department { get; set; } = default!;
+    }
+}
