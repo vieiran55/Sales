315d72640c2a2abb5dcc67510b954c0ffafbbcec
Antonio Vieira <vieira.n55@gmail.com>
2023-12-14 12:46:52 -0300

    Operações assíncronas usando Task (async e await)

--- SalesCurso/Services/DepartmentService.cs
+++ SalesCurso/Services/DepartmentService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesCurso.Data;
 using SalesCurso.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesCurso.Services
 {
@@ -12,9 +13,9 @@ namespace SalesCurso.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
