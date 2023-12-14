723a11d25bdb85b1ebed6a4c035e9ee12b430235
Antonio Vieira <vieira.n55@gmail.com>
2023-12-14 14:39:43 -0300

    Tratamento de exceção para delete (integridade referencial)

---
+++ SalesCurso/Services/Exceptions/IntregityException.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesCurso.Services.Exceptions
+{
+    public class IntregityException : ApplicationException
+    {
+        public IntregityException(string message) : base(message)
+        {
+        }
+    }
+}
