504cb0e4663f072eda7ead0d43d4da97ae93e12f
Antonio Vieira <vieira.n55@gmail.com>
2023-12-14 15:37:16 -0300

    Projeto finalizado

--- SalesCurso/Models/SalesRecord.cs
+++ SalesCurso/Models/SalesRecord.cs
@@ -1,11 +1,15 @@
 ﻿using SalesCurso.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesCurso.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
